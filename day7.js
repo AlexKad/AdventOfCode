5807eb6615207e69c134c92aac41b193193eff1c
Alex <AlexandraMail@yandex.ru>
2016-01-05 22:24:19 +0300

    Day 7 working

--- day7.js
+++ day7.js
@@ -1,28 +1,35 @@
 function createSchema(){
 	var input = $('#input').val().split('\n');
-	var schema = new Schema();
-	schema.setSignals(input);
+	var schema = new Schema(input);
+	//schema.setSignals(input);
 	var res = schema.getSignal('a');
 	$("#res").text("Result is " + res);
 }
 
-function Schema () {
-	this.input = [];
+function Schema (input) {
+	this.input = input;
 	this.signals = [];	
+	while(!this.setSignals()){
+		this.setSignals();
+		console.log(input.length);
+	}
 }
 
-Schema.prototype.setSignals = function(input){
-	this.input = input;
-	var lines = this.input;
-	var line, operator,target, formula;
+Schema.prototype.setSignals = function(){
+	var line, operator, target, formula, signal;
 
-	for(var i=0; i< lines.length; i++){
-		line = lines[i];
+	for(var i=0; i< this.input.length; i++){
+		line = this.input[i];
 		operator = this.getOperator(line);		
 		formula = (line.split(' -> ')[0]).trim();
 		target = (line.split(' -> ')[1]).trim();
-		this.setSignal(formula,target, operator);
+		signal = this.setSignal(formula, operator);
+		if (typeof (signal) !== "undefined") {
+			this.signals[target] = signal;
+			this.input.splice(i, 1);
+        }
 	}
+	return this.input.length===0;	
 }
 
 Schema.prototype.getOperator = function(line){  //operations always uppercase
@@ -31,7 +38,7 @@ Schema.prototype.getOperator = function(line){  //operations always uppercase
 	return false;
 }
 
-Schema.prototype.setSignal = function(formula, target, operator){
+Schema.prototype.setSignal = function(formula, operator){
 	var res;
 	if(operator){
 		res = this.operate(formula, operator);
@@ -45,7 +52,7 @@ Schema.prototype.setSignal = function(formula, target, operator){
 		}
 	}
 	
-	if(res!=undefined) this.signals[target] = res;
+	return res;
 }
 
 Schema.prototype.getSignal = function(target){
@@ -60,8 +67,8 @@ Schema.prototype.operate = function(formula, operator){
 	var args = formula.split(' '), arg1, arg2, res;
 	var max = 65535;
 	var signals = this.signals;
-	arg1 = signals[args[0]];
-	arg2 = signals[args[2]];
+	arg1 = isNaN(parseInt(args[0])) ? signals[args[0]]:parseInt(args[0]);
+	arg2 = isNaN(parseInt(args[2])) ? signals[args[2]]:parseInt(args[2]);
 
      switch(operator){
      	case 'AND':
@@ -81,8 +88,9 @@ Schema.prototype.operate = function(formula, operator){
      		res = arg1>>args[2];
      		break;
      	case 'NOT':
-     		if(signals[args[1]]==undefined) return undefined;
-     		res = signals[args[1]]^max;
+     		arg1 = isNaN(parseInt(args[1])) ? signals[args[1]]:parseInt(args[1]); 
+     		if(arg1==undefined) return undefined;
+     		res = arg1^max;
      		break;	     	
      }
      return res;
