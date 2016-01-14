c4c0273ace10ef43a9c481f3342a29f8cfa4ae6c
Alex Kad <Alexandra_Kadushkina@epam.com>
2016-01-14 18:19:47 +0300

    Day 14 part1 solved

---
+++ day14.js
@@ -0,0 +1,50 @@
+function findBestReindeer(){
+	var input = $('#input').val().split("\n");
+	var time = 2503;
+
+	var deers = createDeersFromInput(input);
+	var winnerDeer = _.max(deers, function(deer){ return deer.res = deer.countDistanceAtTime(time) });
+
+	//var res = raceForTime(deers, time);
+	$("#res").text('The winner is '+ winnerDeer.name+ ' with result ' + winnerDeer.res);
+}
+
+
+
+function createDeersFromInput (input) {
+	var deers = [], parsedLine;
+	input.forEach(function(line){
+		parsedLine = line.match(/(\D+) can fly (\d+) km\/s for (\d+) seconds, but then must rest for (\d+) seconds./);
+		deers.push(new Reinder(parsedLine[1], parsedLine[2], parsedLine[3], parsedLine[4]));
+	});
+	return deers;
+}
+
+function raceForTime(deers,time){
+	for(var i=0;i<deers.length; i++){
+		deers[i].res = deers[i].countDistanceAtTime(time);
+		console.log(deers[i].name + " " + deers[i].res);
+	}
+	 return _.max(deers, function(deer){ return deer.res }).name;
+}
+
+
+function Reinder(name, speed, stopAt, stopLength){
+	this.name =  name;
+	this.speed = speed;
+	this.stopAt = stopAt;
+	this.stopLength = stopLength;
+}
+
+Reinder.prototype.countDistanceAtTime = function(time){
+	var kms = 0;
+	while(time > this.stopAt){
+		kms = kms + this.speed*this.stopAt;
+		time= time - this.stopAt - this.stopLength;
+	}
+	if(time>0){
+		kms = kms+ time*this.speed;
+	}
+	return kms;
+}
+
