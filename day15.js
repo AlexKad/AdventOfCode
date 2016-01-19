3b5590664ae742011d28252d30824aba26e62ebc
Alex Kad <alexandramail@yandex.ru>
2016-01-19 14:33:06 +0300

    Day 15 solved

--- day15.js
+++ day15.js
@@ -1,34 +1,68 @@
+
+
 function countIngridients(){
 	var input = $('#input').val().split('\n');
+	var spoonNum = 100;
 
-	measureIngridients(input);
-	var res="";
+	var res = measureIngridients(input, spoonNum);	
+	
 	$('#res').text('Result is ' + res);
 }
 
-function measureIngridients(input){
-	var parsedIngridientsWithProperties = parseInput(input);
-	findBestAmount(parsedIngridientsWithProperties);
-}
-
-
 function parseInput(input){
-	var ing = [], line, parsed;
-	for(var i=0; i<input.length; i++){
-		line = input[i];
-		parsed = line.match(/(\D+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)/);
-		ing.push({ ing: parsed[1],
-		           capacity: parseInt(parsed[2]),
-		           durability: parseInt(parsed[3]),
-		           flavor: parseInt(parsed[4]),
-		           texture: parseInt(parsed[5]),
-		           calories: parseInt(parsed[6]),
-		           amount:0
-		       });
+	var ing = [], parsed;
+	for(var i=0; i<input.length; i++){		
+		parsed = input[i].match(/-?\d+/g); //find all numbers
+		ing.push(parsed);
 	}
 	return ing;
 }
 
-function findBestAmount(ingr){
+function measureIngridients(input, spoonNum){
+	var ingrs = parseInput(input);
+	return findBestAmount(ingrs, spoonNum);
+}
 
+
+
+var sum = arr => arr.reduce((a, b) => a + b, 0);
+
+// return array with array.Length = arrLength, sum of elements == total 
+function* measure(total, arrLength, res) {	
+    if(res === undefined) res = [];
+
+    var partialSum = sum(res);
+    for (var i = 1; i < total - partialSum - (arrLength - 1) + 1; i++) {
+        var resNew = [...res, i]; //first elem of array
+        if (arrLength > 2) {
+            yield* measure(total, arrLength - 1, resNew);
+        } else {
+            resNew.push(total - partialSum - i);
+            //console.log( resNew.reduce(function(str, el){ return str+ ' ' + el}, "") );
+            yield resNew;
+        }
+    }
+}
+
+
+
+function findBestAmount(ingrs, spoonNum) {
+    var max = -Infinity,
+    	countedIngrs, propSum, score, calories;
+
+    for (var spoonsAmount of measure(spoonNum, ingrs.length)) {
+
+		countedIngrs = ingrs.map((ingr, i) => ingr.map(prop => prop * spoonsAmount[i]));
+        calories = countedIngrs.map(ingr => ingr.pop()); //exclude calories
+
+        
+        propSum = countedIngrs.reduce((sum, el) => sum.map((prop, i) => prop + el[i]));
+        score = propSum.reduce((acc, propSum) => acc * Math.max(propSum, 0), 1);
+
+        if (score > max) {
+            max = score;
+        }
+    }
+
+    return max;
 }
