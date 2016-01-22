23bec8f8ee19495180483c6089218007612b9b9f
Alex Kad <alexandramail@yandex.ru>
2016-01-22 18:31:05 +0300

    Day 17.js solved

--- day17.js
+++ day17.js
@@ -1,15 +1,22 @@
 function fillingContainers(){
-	var containers = $('#input').val().split('\n');
-	var liters = 150;
-
-	findCombinations(containers,liters);
-
-	var res = 0;
+	containers = $('#input').val().split('\n').map(cont => parseInt(cont));
+	var liters = 150;	
+	var res = findCombinations(liters, containers.length);	
 	$('#res').text('There are '+ res+ ' combinations');
 }
 
-var sum = arr => arr.reduce((a, b) => a + b, 0);
+var containers;
 
-function findCombinations(containers, liters){
-	var res = [];
+function findCombinations(liters, length, i){
+	i = i || 0;
+	if(length<0){
+		return 0;
+	} else if(liters ===0){
+		return 1;
+	}
+	else if(i === containers.length || liters < 0){
+		return 0;
+	} else {
+		return findCombinations(liters, length, i+1) + findCombinations(liters - containers[i], length-1, i+1);
+	}
 }
